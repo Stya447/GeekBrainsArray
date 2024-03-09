ff5d103a73d00f08dc55912e340efa1e80bffea7
Stya447 <asyaphilip447@gmail.com>
2024-03-09 19:11:36 +0300

    Добавлено решение задачи.

--- GeekBrainsArray/Program.cs
+++ GeekBrainsArray/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+Console.Write("Введите массив строк через запятую: ");
+string[] input = Console.ReadLine().Split(",");
+GetElementsWithLengthEqualsOrLessThan3(input);
+Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+string[] GetElementsWithLengthEqualsOrLessThan3(string[] args)
+{
+    string[] newArgs = new string[args.Length];
+    int counter = 0;
+    for (int i = 0; i < args.Length; i++)
+    {
+        if (args[i].Length <= 3)
+        {
+            newArgs[counter] = args[i];
+            counter++;
+        }
+    }
+    
+    Console.Write("Ответ: ");
+    for(int i=0;i< newArgs.Length;i++)
+        Console.Write(newArgs[i] + " ");
+    Console.WriteLine(); 
+    
+    return newArgs;
+}
